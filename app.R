2e6abce4ea9b730b2f1a0243d741be49fa93a21c
Joe Ledford <jtledford3@icloud.com>
2026-03-27 08:55:14 -0400

    Fixed app to based on new instructions.

--- app.R
+++ app.R
@@ -1,21 +1,47 @@
+# Joe Ledford
+
 library(shiny)
 
-# Global variables can go here
+# Global variables
 n <- 200
-
 
 # Define the UI
 ui <- bootstrapPage(
   h1("Joe Ledford"),
-  numericInput('n', 'Number of obs', n),
-  plotOutput('plot')
+  
+  numericInput(
+    inputId = "n",
+    label = "Number of observations",
+    value = n
+  ),
+  
+  selectInput(
+    inputId = "plot_type",
+    label = "Select Plot Type",
+    choices = c("Histogram", "Boxplot"),
+    selected = "Histogram"
+  ),
+  
+  selectInput(
+    inputId = "plot_color",
+    label = "Select Plot Color",
+    choices = c("Black" = "black", "Red" = "red", "Blue" = "blue", "Green" = "green"),
+    selected = "black"
+  ),
+  
+  plotOutput("plot")
 )
-
 
 # Define the server code
 server <- function(input, output) {
   output$plot <- renderPlot({
-    boxplot(runif(input$n), col="red")
+    data <- runif(input$n)
+    
+    if (input$plot_type == "Histogram") {
+      hist(data, col = input$plot_color, main = "Histogram", xlab = "Value")
+    } else {
+      boxplot(data, col = input$plot_color, main = "Boxplot")
+    }
   })
 }
 
